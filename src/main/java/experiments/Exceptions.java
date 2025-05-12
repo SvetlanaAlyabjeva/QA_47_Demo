420d90c507f1970734d12d4d4d70d21a7af0a5b7
lanaal <svetlana.alyabjeva@gmail.com>
2025-05-12 20:26:13 +0300

    created exceptions

--- src/main/java/experiments/Exceptions.java
+++ src/main/java/experiments/Exceptions.java
@@ -1,4 +1,25 @@
 package experiments;
 
 public class Exceptions {
+    public static void main(String[] args) {
+        String[] strArray = {"str1", "str2", "str3"};
+        uncheckedException(strArray);
+    }
+
+    private static void uncheckedException(String[] strArray) {
+        try {
+            for (int i = 0; i < 5; i++) {
+                System.out.println(strArray[i]);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+            System.out.println("create exception");
+        }
+        System.out.println("program is working");
+
+    }
+               private static void pause(){
+        Thread.sleep(millis:3000);
+
+    }
 }
