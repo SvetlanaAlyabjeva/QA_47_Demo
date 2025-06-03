f458aa000534539233746295f72bc325f7e0417a
lanaal <svetlana.alyabjeva@gmail.com>
2025-06-03 20:57:59 +0300

    created project

---
+++ src/test/java/tests/PracticeFormTests.java
@@ -0,0 +1,16 @@
+package tests;
+
+import manager.AppManager;
+import org.testng.annotations.Test;
+import pages.FormsPage;
+import pages.HomePage;
+import pages.PracticeFormPage;
+
+public class PracticeFormTests extends AppManager {
+    @Test
+    public void studentRegFormPositive(){
+new HomePage(getDriver()).clickBtnForms();
+new FormsPage(getDriver()).clickBtnPracticeForm();
+new PracticeFormPage(getDriver()).typePracticeForm();
+    }
+}
